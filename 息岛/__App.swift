c2c695e7b7879dc1c7fd6ee4b60421a46995430a
TQing <mantq@qq.com>
2026-03-17 09:35:18 +0800

    Initial Commit

---
+++ 息岛/__App.swift
@@ -0,0 +1,17 @@
+//
+//  __App.swift
+//  息岛
+//
+//  Created by Qing on 2026/3/17.
+//
+
+import SwiftUI
+
+@main
+struct __App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ 息岛Tests/__Tests.swift
@@ -0,0 +1,16 @@
+//
+//  __Tests.swift
+//  息岛Tests
+//
+//  Created by Qing on 2026/3/17.
+//
+
+import Testing
+
+struct __Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
